da59dd7344ca365735834768650ef5f7930034d6
Daniel McLean <dmclean93@gmail.com>
2020-04-05 15:34:47 -0400

    Initial Commit

---
+++ DrawwnCam/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DrawwnCam
+//
+//  Created by Daniel McLean on 4/5/20.
+//  Copyright © 2020 Daniel McLean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DrawwnCam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DrawwnCam
+//
+//  Created by Daniel McLean on 4/5/20.
+//  Copyright © 2020 Daniel McLean. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
